0157caec5fd242060a194d6a80a4a7a6813d5390
josef kvapil <josef0kvapil@gmail.com>
2021-05-06 19:05:30 +0200

    hurikan

--- commands/hurikan.js
+++ commands/hurikan.js
@@ -1,8 +1,8 @@
 import playYoutube from "../utils/playYoutube";
 
 module.exports = {
-    name: 'sposrals',
-    description: 'Tos posral',
+    name: 'hurikan',
+    description: 'Říkal jsi hurikán',
     execute(msg, args) {
         console.log("sposrals");
         playYoutube(msg, 'https://www.youtube.com/watch?v=2WhfVeHU6Uw')
